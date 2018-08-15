06032a19e91a51ac4a17a1aed1ea82578e80fdfb
manish <manghwani.manish1996@gmail.com>
2018-08-15 11:56:22 +0530

    cookies added

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 require('./models/User');
 require('./services/passport');
 
 
-mongoose.connect('')
+mongoose.connect(keys.mongoURI);
 
-const app = express(keys.mongoURI);
+const app = express();
+
+app.use(cookieSession({
+        maxAge : 1*24*60*60*1000,
+        keys : [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 // app.use( passport.initialize());
--- services/passport.js
+++ services/passport.js
@@ -5,13 +5,32 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null,user.id);
+});
+
+passport.deserializeUser((id, done)=>{
+    User.findById(id).then(user =>{
+        done(null, user);
+    });
+});
+
 passport.use(
     new GoogleStategy({
         clientID : keys.googleClientID,
         clientSecret : keys.googleClientSecret,
-        callbackURL : '/auth/google/callback',
-    },(accessToken , refreshToken , profile, done) => {
-        new User({googleID : profile.id}).save();
+        callbackURL : 'http://localhost:5000/auth/google/callback',
+    },(accessToken , refreshToken , profile, cb) => {
+        
+        User.findOne({googleID : profile.id}).then(existingUser => {
+            if(existingUser){
+                done(null,existingUser);
+                console.log(googleID);
+            }else{
+                new User({googleID : profile.id}).save()
+                .then(user => done(null,user));
+            }
+        })
     })
 );
 
